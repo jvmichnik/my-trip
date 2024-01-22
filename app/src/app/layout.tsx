49950a3d7b42433b9503ec7e126c17f0a43b631a
João Vitor <joaovitormck@gmail.com>
2024-01-21 21:10:33 -0300

    fix: export layout

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils";
 import "./globals.css";
 import "../lib/dayfns";
 
-export const fontSans = FontSans({
+const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
